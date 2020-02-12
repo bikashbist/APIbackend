113fa0d745bf6b7195aef8dd5c85b03ae87e4ef8
dell <bikas.bist@yahoo.com>
2020-02-12 14:17:50 +0545

    hasing password

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const User = require('../models/users');
 
 
+
+//for register
 router.post('/signup', (req, res, next) => {
 
     //first checks if username exist
@@ -17,16 +19,16 @@ router.post('/signup', (req, res, next) => {
             }
 
 
-            //hasing password
-            // bcrypt.hash(req.body.password, 10, function (err, hash) {
-            //     if (err) {
-            //         throw new Error('Could not encrypt password!');
-            //     }
+           // hasing password
+            bcrypt.hash(req.body.password, 10, function (err, hash) {
+                if (err) {
+                    throw new Error('Could not encrypt password!');
+                }
 
                 //create user
                 User.create({
                     username: req.body.username,
-                    password: req.body.password,
+                    password: hash,
                     firstName: req.body.firstName,
                     lastName: req.body.lastName,
                     image:req.body.image,
@@ -39,7 +41,29 @@ router.post('/signup', (req, res, next) => {
                 }).catch(next);
             });
         });
-// });
+});
 
+
+
+//for login
+router.post('/login', (req, res, next) => {
+    User.findOne({ username: req.body.username })
+        .then((user) => {
+            if (user === null) {
+                let err = new Error('User not found!');
+                err.status = 401;
+                return next(err);
+            }
+            bcrypt.compare(req.body.password, user.password, function (err, status) {
+                if (!status) {
+                    let err = new Error('Password does not match!');
+                    err.status = 401;
+                    return next(err);
+                }
+                let token = jwt.sign({ userId: user._id }, process.env.SECRET);
+                res.json({ status: 'Login Successful!', token: token });
+            });
+        }).catch(next);
+});
 
 module.exports = router;
